41aab3830efb25edb06850e6f0b1a509920d8ee3
lullii <soylpe20@gmail.com>
2023-06-08 16:24:22 +0300

    class main and readme.md

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] sales = new int[] {1, 4, 10, 15};
+        SalesManager salesManager = new SalesManager(sales);
+
+        int max = salesManager.max();
+
+        System.out.println(max);
     }
 }
